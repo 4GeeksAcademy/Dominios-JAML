57ef63380a838fc054c4381095608801ef29cf2a
Alessander Leon <alessanderleon@gmail.com>
2025-02-26 01:21:04 +0000

    Terminado el generador de nombres de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+function Generador() {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension = ['.com', '.es', '.ve']
+  let nombre = "";
+  
 
+  for (let i = 0; i < 8; i++) {
+    nombre = pronoun[Math.floor(Math.random() * pronoun.length)] + adj[Math.floor(Math.random() * adj.length)] + noun[Math.floor(Math.random() * noun.length)] + extension[Math.floor(Math.random() * extension.length)]
+    console.log(nombre)
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+Generador()
